9ede1a2900e9444513730b056be22031598a3917
Devin Lewtan <devinlewtan@gmail.com>
2020-04-19 11:35:23 -0400

    Handle the Form Submission and Modify the User Interface

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+
 // app.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+  const port = 3000;
+  app.listen(port);
+  console.log(`server started on port ${port}`);
